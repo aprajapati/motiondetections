66b2b112f199ff6471c188190eb5f653d4c80396
aprajapati <ashish.prajapati@gmail.com>
2015-12-23 22:23:56 -0800

    Added logic to alert in case of motion detection

--- motionDetectionWithBkgSubtractor.py
+++ motionDetectionWithBkgSubtractor.py
@@ -1,4 +1,17 @@
+import argparse
 import cv2
+import time
+import os
+
+# Globals
+moveQualified = 0
+
+# Arguments business
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the video file")
+ap.add_argument("-n", "--no_display", action='store_true', help="do not show video display")
+ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+args = vars(ap.parse_args())
 
 cap = cv2.VideoCapture(0)
 
@@ -12,22 +25,33 @@ while(1):
     (_, cnts, _) = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)
 
+    moveQualified = 0
     # loop over the contours
     for c in cnts:
         # if the contour is too small, ignore it
-        if cv2.contourArea(c) < 255:
+        if cv2.contourArea(c) < args["min_area"]:
             continue
+
+        moveQualified = 1
  
         # compute the bounding box for the contour, draw it on the frame,
         # and update the text
         (x, y, w, h) = cv2.boundingRect(c)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+    # Quit logic for window and non window mode
+    if args["no_display"] is not True:
+        # Display the resulting frame
+        cv2.imshow('frame',frame)
 
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        print "Quitting"
-        break;
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            print "Quitting"
+            break;
+    else:
+        if moveQualified == 1:
+                os.system("espeak 'Oye'")
+                moveQualified = 0
+
 cap.release()
 cv2.destroyAllWindows()
 
